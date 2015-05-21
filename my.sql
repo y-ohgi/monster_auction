4c4b5d099e0f24735c23a13dcef2e63ce9fe37cb
y-ohgi <ohgi.211@gmail.com>
2015-05-21 17:57:05 +0900

    gamewait途中

--- my.sql
+++ my.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `user_master` (
   `um_id` int(11) NOT NULL AUTO_INCREMENT,
-  `um_rm_id` int(11), -- あとでFKを貼る
+  `um_rm_id` int(11), -- TODO: あとでFKを貼る
   `um_name` varchar(255) NOT NULL,
   `um_uuid` varchar(255),
+  `um_active` timestamp, -- ユーザーのアクティブチェック
   `um_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`um_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci;
