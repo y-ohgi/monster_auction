a87060d4312414d88926a4c2ece9a77e68b9c15f
y-ohgi_serv <root@v157-7-114-70.z1d14.static.cnode.jp>
2015-05-21 12:53:19 +0900

    sql文の修正

--- my.sql
+++ my.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `user_master` (
   `um_id` int(11) NOT NULL AUTO_INCREMENT,
+  `um_rm_id` int(11), -- あとでFKを貼る
   `um_name` varchar(255) NOT NULL,
   `um_uuid` varchar(255),
   `um_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
