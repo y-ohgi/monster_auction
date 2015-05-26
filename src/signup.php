7d4a3246d461971f93f5e5c665ce79fe4510456b
y-ohgi <ohgi.211@gmail.com>
2015-05-26 16:41:38 +0900

    signup.phpの改修完了

--- src/signup.php
+++ src/signup.php
@@ -1,14 +1,13 @@
 <?php
 
-//require_once('init.php');
 require_once('controller/Util.inc');
-//require_once('db_connect.php');
 require_once('model/Userdb.inc');
 require_once('controller/Page.inc');
 
 // req:
 $name = trim(Util::h(@$_POST['name']));
 $uuid = Util::h(@$_POST['uuid']);
+
 // res:
 $response = array(
     "result"=>null,
@@ -18,22 +17,18 @@ Page::setRes($response);
 
 
 if($name == ''){
-    Page::complete(false, '何も入力されていません');
+    Page::complete(false, '名前が入力されていません');
 }
 
 // db処理
 try{
-    Db::getDbh()->beginTransaction();
-
-    if(Userdb::regUser($uuid, $name) !== true){
-        Page::complete(false, '名前が存在しています');
+    $register = Userdb::regUser($uuid, $name);
+    if($register !== true){
+        Page::complete(false, $register);
     }
-    Db::getDbh()->commit();
 }catch(Exception $error){
     Db::getDbh()->rollback();
-    Db::disconnect();
     Page::complete(false, 'db error');
 }
 
 Page::complete(true, 'Success!!');
-//echo "登録完了";
