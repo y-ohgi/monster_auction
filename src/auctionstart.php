6af5a5046bede9514a8560cf7739769b08b71854
y-ohgi <ohgi.211@gmail.com>
2015-06-03 19:18:34 +0900

    auctionstart.phpの作成

---
+++ src/auctionstart.php
@@ -0,0 +1,80 @@
+<?php
+// gameauction.php
+
+require_once('controller/Page.inc');
+require_once('controller/Util.inc');
+require_once('controller/Time.inc');
+require_once('controller/User.inc');
+require_once('model/RoomDao.inc');
+//require_once('model/ActiveDao.inc');
+require_once('model/UserDao.inc');
+
+// req:
+$uuid = Util::h($_POST['uuid']);
+// res:
+$response = array(
+    "status"=>null,
+    "timer"=>null
+);
+Page::setRes($response);
+
+if(UserDao::authUser($uuid) !== true){
+    Page::complete(452);
+}
+
+$user = new User($uuid);
+$ua_id = $user->getUAid();
+if(!$ua_id){
+    Page::complete(453);
+}
+//$ru_id = $user->getRUid();
+$rm_id = $user->getRMid();
+
+$timer;
+
+
+try{
+    /*
+    // 人数差を求める
+    $sql = 'SELECT * FROM room_master WHERE rm_id = :rm_id;';
+    if($row['rm_ppl'] === $row['rm_max']){}
+     */
+    
+    $sql = 'SELECT * FROM room_auction WHERE ra_rm_id = :rm_id;';
+    $stmt = Dbh::get()->prepare($sql);
+    $stmt->bindValue(':rm_id', $rm_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    // auctionが存在しなかった場合新たに作成
+    if(!$row){
+        $sql = 'INSERT INTO room_auction(ra_rm_id) VALUES(:rm_id);';
+        $stmt = Dbh::get()->prepare($sql);
+        $stmt->bindValue(':rm_id', $rm_id, PDO::PARAM_INT);
+        $stmt->execute();
+        
+        $sql = 'SELECT * FROM room_auction WHERE ra_rm_id = :rm_id;';
+        $stmt = Dbh::get()->prepare($sql);
+        $stmt->bindValue(':rm_id', $rm_id, PDO::PARAM_INT);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    $created = Carbon::parse($row['ra_created']);
+    $targettime = $created->copy()->addSeconds(Time::getAuctionStart());
+    // Time::AuctionStart()秒分経ったか
+    if($targettime->isPast()){
+        $timer = 0;
+    // 残り秒を求める
+    }else{
+        $timer = $created->diffInSeconds($targettime);
+    }
+
+
+    
+}catch(Exception $e){
+    //echo $e->getMessage();
+    Page::complete(550);
+}
+
+Page::complete(200, $timer);
+
--- src/hoge.php
+++ src/hoge.php
@@ -1,9 +1,57 @@
 <?php
 
-require_once('db_connect.php');
+require_once(dirname(__FILE__).'/../lib/Carbon/Carbon.php');
+use Carbon\Carbon;
 
-$sql = "SELECT * FROM user_master ORDER BY um_id DESC;";
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
-var_dump($row);
+require_once('controller/Time.inc');
+
+
+function b(){
+    echo "<br />";
+}
+
+$time = "2015-06-03 17:25:57";
+
+$created = Carbon::parse($time);
+//$created = Carbon::now();
+$targettime = $created->copy()->subSeconds(Time::getAuctionStart());
+
+echo $diff = $created->diffInSeconds($targettime);
+/*
+// Time::AuctionStart()秒分経ったか
+if($targettime->isPast()){
+    echo "Past";
+    // 残り秒を求める
+}else{
+    echo $diff = $created->diffInSeconds($targettime);
+}
+
+/*
+$time = "2015-06-03 17:25:57";
+
+$test = Carbon::parse($time);
+echo $test->timestamp;
+b();
+$now = Carbon::now();
+echo $test->diffInSeconds($now);
+b();
+
+echo $test;
+b();
+var_dump($test->addSecond(10)->isPast());
+var_dump($now->addSecond(10)->isPast());
+b();
+/**/
+/*
+// //$dtOttawa = Carbon::createFromDate(2000, 1, 1, 'America/Toronto');
+// $dtOttawa = Carbon::parse($time);
+// //$dtVancouver = Carbon::createFromDate(2000, 1, 1, 'America/Vancouver');
+// $dtVancouver = Carbon::now();
+// echo $dtOttawa->diffInMinutes($dtVancouver);
+// echo $dtOttawa->diffInSeconds($dtVancouver);
+
+// b();
+
+echo(Carbon::now());
+
+*/
